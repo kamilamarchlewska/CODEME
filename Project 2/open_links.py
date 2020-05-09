4186728f42a77611cf5fdd31104ff1f9eadac66d
kamilamarchlewska <61703747+kamilamarchlewska@users.noreply.github.com>
2020-05-09 23:33:45 +0200

    delete
    delete

--- Project 2/open_links.py
+++
@@ -1,7 +0,0 @@
-filename = 'AffiliateLink.txt'
-with open(filename, 'r') as singleLink: # Read links from the file - AffiliateLink.txt 
-    allLinks = singleLink.readlines()
-
-    for line in allLinks: #displays links
-        line = line.replace('\n', '')
-        print('► ' + line)
